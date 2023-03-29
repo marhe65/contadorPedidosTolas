51313a012ab9435f3d10fcdd14d6850af6efb754
Martin Luna <mhluna6519@gmail.com>
2023-03-28 19:49:35 -0500

    creando botones de clic y reinicio

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import './App.css';
+import Boton from './componentes/Boton';
 import logoTolas from './images/Logo-Las-Tolas.png'
 
 function App() {
+
+  const manejarClic = () => {
+    console.log('Clic');
+  }
+
+  const reiniciarContador = () => {
+    console.log('Reiniciar');
+  }
   return (
     <div className="App">
       <div className='tolas-logo-contenedor'>
         <img className='tolas-logo'
         src={logoTolas}
         alt='Logo de las Tolas' />
+      </div>
+      <div className='contenedor-principal'>
+        <Boton texto='Clic' esBotonDeClic={true} manejarClic={manejarClic} />
+        <Boton texto='Reiniciar' esBotonDeClic={false} manejarClic={reiniciarContador} />
       </div>
     </div>
   );
