5374b3f4694263ffe86c5f648cf55fb8321360df
Martin Luna <mhluna6519@gmail.com>
2023-03-29 15:13:00 -0500

    Agregar campo contador, estilizarlo y configurar el useState inicial

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import './App.css';
 import Boton from './componentes/Boton';
-import logoTolas from './images/Logo-Las-Tolas.png'
+import logoTolas from './images/Logo-Las-Tolas.png';
+import Contador from "./componentes/Contador";
+/*debemos importar el Hook useState para dar un estado inicial al componente funcional */
+import { useState  } from "react";
 
 function App() {
+/*creamos una cte que es un array con 2 elementos, el primero es el valor que queremos usar como
+el estado y luego una función que nos vá a permitir actualizar dicho valor...los nombres los colocamos nos,
+colocamos set(asignar) y el nombre del estado que hayamos determinado */
+  const [numClics, setNumClics] = useState(0);/*cero es el valor inicial del estado */
 
   const manejarClic = () => {
-    console.log('Clic');
-  }
-
+    //console.log('Clic');
+    setNumClics(numClics + 1);
+  };
   const reiniciarContador = () => {
-    console.log('Reiniciar');
-  }
+    //console.log('Reiniciar');
+    setNumClics(0);
+  };
   return (
     <div className="App">
       <div className='tolas-logo-contenedor'>
@@ -19,6 +27,7 @@ function App() {
         alt='Logo de las Tolas' />
       </div>
       <div className='contenedor-principal'>
+        <Contador numClics={ numClics } />
         <Boton texto='Clic' esBotonDeClic={true} manejarClic={manejarClic} />
         <Boton texto='Reiniciar' esBotonDeClic={false} manejarClic={reiniciarContador} />
       </div>
