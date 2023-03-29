99451c280793c088d7c2063e990add3aa3f4f60d
Martin Luna <mhluna6519@gmail.com>
2023-03-29 11:18:31 -0500

    Estilizar los botones

--- src/componentes/Boton.js
+++ src/componentes/Boton.js
@@ -1,10 +1,12 @@
 import React from "react";
+import '../stylesheets/Boton.css';
 
 function Boton({ texto, esBotonDeClic, manejarClic }) {/*aplicando la desestructuración evitamos usar props y usamos directamente
 el valor del parametro en el botón */
   return(
     /*el className del botón depende de la función a realizar, si es de clic o de reinicio, entonces 
     en los parametros agregamos esBotonDeClic y usamos un operador ternario*/
+    /*si el boton es clic => la clase es boton-clic, si no boton-reiniciar */
     <button className={esBotonDeClic ? 'boton-clic' : 'boton-reiniciar'}
             onClick={manejarClic}>
       {texto}
